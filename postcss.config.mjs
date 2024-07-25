225c869869c1f691e7502555324200a04c895038
rashidamahroof <rashidamahroofp@gmail.com>
2024-07-25 14:06:06 +0400

    df

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,22 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      keyframes: {
-        slideUp: {
-          '0%': { transform: 'translateY(100%)', opacity: 0 },
-          '100%': { transform: 'translateY(0)', opacity: 1 },
-        },
-      },
-      animation: {
-        slideUp: 'slideUp 0.5s ease-out',
-      },
-    },
-  },
+  
+  
   plugins: {
     tailwindcss: {},
   },
